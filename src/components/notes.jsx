2d0d3f3a1d2b115d5ce4ea57a92c92cf6bf7a642
Aкѕнαу <akshaypmna18@gmail.com>
2023-12-11 20:10:34 +0530

    read,write- data working

--- src/components/notes.jsx
+++ src/components/notes.jsx
@@ -1,8 +1,8 @@
 import Theme from "./theme";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth, db } from "../lib/firebase";
-import { ref, set, push } from "firebase/database";
+import { ref, set, push, onValue } from "firebase/database";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
@@ -29,6 +29,7 @@ import { Textarea } from "./ui/textarea";
 
 function Notes() {
   const [uid, setUid] = useState("");
+  const [username, setUsername] = useState("");
   const [notes, setNotes] = useState([]);
   const navigate = useNavigate();
   const form = useForm();
@@ -38,9 +39,17 @@ function Notes() {
       if (user) {
         const uid = user.uid;
         setUid(uid);
+        setUsername(auth.currentUser.email.replace(/[^a-zA-Z0-9]/g, ""));
+        fetchNotes();
       }
     });
   }, []);
+
+  const fetchNotes = () => {
+    onValue(ref(db, `/notes/`), (snapshot) => {
+      setNotes(snapshot.val());
+    });
+  };
 
   const onLogout = () => {
     signOut(auth).then(() => {
@@ -49,7 +58,6 @@ function Notes() {
   };
 
   const addNotes = (data) => {
-    const username = auth.currentUser.email.replace(/[^a-zA-Z0-9]/g, "");
     const userNotesRef = ref(db, `/notes/${username}/`);
     const newNoteRef = push(userNotesRef);
     set(newNoteRef, {
@@ -57,24 +65,13 @@ function Notes() {
       note: data.note,
     });
     form.reset();
+    fetchNotes();
   };
 
   return (
     <div className="space-y-8">
       <Theme />
       <p>User id = {uid}</p>
-      <div className="space-y-4">
-        <h3 className="font-bold text-2xl">Notes</h3>
-        {notes.map(({ title, note }, index) => {
-          return (
-            <div key={index} className="border p-4 mx-4">
-              <p className="font-semibold capitalize">{title}</p>
-              <Separator className="max-w-[10rem] mt-2 mb-3" />
-              <p>{note}</p>
-            </div>
-          );
-        })}
-      </div>
       <Dialog>
         <DialogTrigger asChild>
           <Button>Add new note</Button>
@@ -125,6 +122,18 @@ function Notes() {
         </DialogContent>
       </Dialog>
       <br />
+      <div className="space-y-4">
+        <h3 className="font-bold text-2xl">Notes</h3>
+        {notes[username]
+          ? Object.entries(notes[username]).map(([noteId, { title, note }]) => (
+              <div key={noteId} className="border p-4 mx-4">
+                <p className="font-semibold capitalize">{title}</p>
+                <Separator className="max-w-[10rem] mt-2 mb-3" />
+                <p>{note}</p>
+              </div>
+            ))
+          : ""}
+      </div>
       <Button onClick={onLogout}>Logout</Button>
     </div>
   );
