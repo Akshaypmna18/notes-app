e9b5f83ca95bcc520487e27886983c85d7d823fd
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-01-09 20:54:27 +0530

    feat: :sparkles: implement search functionality
    This commit introduces the "Search Notes" feature, enhancing the user experience by allowing efficient searching within the notes. Users can now easily find specific notes by entering keywords in the search bar.

--- src/features/notes/NotesComp.jsx
+++ src/features/notes/NotesComp.jsx
@@ -2,16 +2,27 @@ import React, { useState, useEffect } from "react";
 import { Separator } from "@/components/ui/separator";
 import { TrashIcon } from "@radix-ui/react-icons";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 import DialogModal from "@/components/DialogModal";
 import DialogForm from "@/features/notes/DialogForm";
 import { deleteNote } from "@/features/notes/functions";
 import { useNotes } from "@/store";
 import DeleteNotes from "./DeleteNotes";
+import { Cross1Icon } from "@radix-ui/react-icons";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 function NotesComp({ notesArray, username, fetchNotes, setNoteId }) {
   const { setIsDialogModalOpen } = useNotes((state) => state);
   const [isChecked, setIsChecked] = useState([]);
   const [notesId, setNotesId] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
+  const filteredNotes = notesArray.filter((note) =>
+    note[1].note.toLowerCase().includes(filterValue.toLowerCase())
+  );
+
+  const handleInputChange = (e) => {
+    setFilterValue(e.target.value);
+  };
 
   const handleCheckboxClick = (index, noteId) => {
     setIsChecked((prevChecked) =>
@@ -26,14 +37,50 @@ function NotesComp({ notesArray, username, fetchNotes, setNoteId }) {
     });
   };
 
+  const highlightMatches = (text) => {
+    if (filterValue.trim() === "") {
+      return <span>{text}</span>; // No highlighting when filterValue is blank
+    }
+
+    const regex = new RegExp(`(${filterValue})`, "gi");
+    return text.split(regex).map((part, index) =>
+      regex.test(part) ? (
+        <span key={index} className="bg-yellow-600">
+          {part}
+        </span>
+      ) : (
+        part
+      )
+    );
+  };
+
   useEffect(() => {
     setIsChecked(Array(notesArray.length).fill(false));
   }, [notesArray]);
 
   return (
-    <div className="columns-2xs mt-8">
-      {notesArray.length > 0 &&
-        notesArray.map(([noteId, { title, note }], index) => (
+    <section className="columns-2xs mt-8">
+      <div className="absolute top-16 min-[450px]:top-20 min-[640px]:top-9 left-[50%] translate-x-[-50%]">
+        <span className="absolute top-[0.85rem] left-3">
+          <MagnifyingGlassIcon />
+        </span>
+        <Input
+          placeholder="Search notes..."
+          onChange={handleInputChange}
+          value={filterValue}
+          className="px-8 rounded-full"
+        />
+        <span
+          className={`cursor-pointer absolute top-[0.85rem] right-4 ${
+            filterValue.length > 0 ? "" : "hidden"
+          }`}
+          onClick={() => setFilterValue("")}
+        >
+          <Cross1Icon />
+        </span>
+      </div>
+      {filteredNotes.length > 0 &&
+        filteredNotes.map(([noteId, { title, note }], index) => (
           <DialogModal
             key={index}
             title="Update note"
@@ -48,7 +95,7 @@ function NotesComp({ notesArray, username, fetchNotes, setNoteId }) {
             )}
           >
             <div
-              className="border p-4 rounded-md max-w-full m-2 hover:border-primary cursor-pointer inline"
+              className="border p-4 rounded-md max-w-full m-2 max-[640px]:mt-6 hover:border-primary cursor-pointer inline"
               onClick={() => {
                 setIsDialogModalOpen();
                 setNoteId(noteId);
@@ -75,7 +122,7 @@ function NotesComp({ notesArray, username, fetchNotes, setNoteId }) {
                 </span>
               </p>
               <Separator className="mt-2 mb-3" />
-              <p className="break-words">{note}</p>
+              <p className="break-words">{highlightMatches(note)}</p>
             </div>
           </DialogModal>
         ))}
@@ -84,7 +131,7 @@ function NotesComp({ notesArray, username, fetchNotes, setNoteId }) {
         notesId={notesId}
         setNotesId={setNotesId}
       />
-    </div>
+    </section>
   );
 }
 
