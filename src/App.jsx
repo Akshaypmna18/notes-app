4bdcc845cad8470f2f9cca8abd71f861353e5db0
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-01-16 16:22:51 +0530

    chore: add protected routes with comments, store latest code in GitHub
    Added protected routes to the codebase, but they are currently commented out. Identified a bug that still needs to be addressed. This commit is made to store the latest code in the GitHub repository.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Login from "./pages/authentication/Login";
 import ForgotPassword from "./pages/authentication/ForgotPassword";
 import Signup from "./pages/authentication/Signup";
 import Notes from "./pages/Notes";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+// import { useNotes } from "@/store";
 
 function App() {
+  // const ProtectedRoutes = () => {
+  //   const { user } = useNotes((state) => state);
+  //   return user ? <Outlet /> : <Navigate to="/login" />;
+  // };
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/signup" element={<Signup />} />
+      {/* <Route element={<ProtectedRoutes />}> */}
       <Route path="/notes" element={<Notes />} />
+      {/* </Route> */}
       <Route path="/*" element={<NotFound />} />
     </Routes>
   );
