ff5a73ea2d894d3382a80de65385fb6dbf65ba26
Aкѕнαу <akshaypmnacoc@gmail.com>
2024-01-17 17:55:10 +0530

    refactor: :art: move component functions to hook
    The functions of the existing component have been refactored and moved to a new hook component.

--- src/features/notes/NotesComp.jsx
+++ src/features/notes/NotesComp.jsx
@@ -1,56 +1,22 @@
-import { useState, useEffect, useRef } from "react";
 import { Separator } from "@/components/ui/separator";
 import { Checkbox } from "@/components/ui/checkbox";
 import DialogModal from "@/features/notes/components/DialogModal";
 import DialogForm from "@/features/notes/components/DialogForm";
-import { useNotes } from "@/store";
 import DeleteSingleNote from "./deleteNotes/singleNote";
 import DeleteMultipleNotes from "./deleteNotes/multipleNotes";
+import useNotesComp from "./hooks/useNotesComp";
 
 function NotesComp({ notesArray }) {
-  const { setIsDialogModalOpen, filterValue, setNoteId } = useNotes(
-    (state) => state
-  );
-  const [isChecked, setIsChecked] = useState([]);
-  const [notesId, setNotesId] = useState([]);
-
-  const filteredNotes = notesArray.filter((item) =>
-    item[1].note.toLowerCase().includes(filterValue.toLowerCase())
-  );
-
-  const handleCheckboxClick = (index, noteId) => {
-    setIsChecked((prevChecked) =>
-      prevChecked.map((item, pos) => (pos === index ? !item : item))
-    );
-    setNotesId((prevNotesId) => {
-      if (isChecked[index]) {
-        return prevNotesId.filter((id) => id !== noteId);
-      } else {
-        return [...prevNotesId, noteId];
-      }
-    });
-  };
-
-  const highlightMatches = (text) => {
-    if (filterValue.trim() === "") {
-      return <span>{text}</span>;
-    }
-    const regex = new RegExp(`(${filterValue})`, "gi");
-    return text.split(regex).map((part, index) =>
-      regex.test(part) ? (
-        <span key={index} className="bg-yellow-600">
-          {part}
-        </span>
-      ) : (
-        part
-      )
-    );
-  };
-
-  useEffect(() => {
-    setIsChecked(Array(notesArray.length).fill(false));
-    setNotesId([]);
-  }, [notesArray, filterValue]);
+  const {
+    filteredNotes,
+    setIsDialogModalOpen,
+    setNoteId,
+    isChecked,
+    handleCheckboxClick,
+    highlightMatches,
+    setNotesId,
+    notesId,
+  } = useNotesComp(notesArray);
 
   return (
     <section className="columns-2xs max-[640px]:pt-7">
